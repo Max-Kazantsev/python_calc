78848a9777b8cbbf743a11a5c8ad9e019e63b602
JustAccount1702 <mkgolitsino@mail.ru>
2019-12-07 00:21:28 +0300

    Первые подвижки с pz1

--- pz1/pz1.py
+++ pz1/pz1.py
@@ -1,2 +1,13 @@
 """ Пример решения pz1 """
 
+
+def my_sum(a, b):
+    return a + b
+
+
+if __name__ == '__main__':
+    # Осторожно, Python3.8
+    while ans := '' != 'exit':
+        ans = input('Введите пример:')
+        a, b = ans.split('+')
+        print(f"По-моему это: {my_sum(int(a), int(b))}")
