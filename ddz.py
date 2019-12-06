30d28fba1821f84e602608f5b5b4db8fbaaca599
JustAccount1702 <mkgolitsino@mail.ru>
2019-12-06 23:46:45 +0300

    Initial commit

---
+++ ddz.py
@@ -0,0 +1,46 @@
+""" Калькулятор на основе словаря """
+import re
+
+
+def my_diff(*args):
+    result = args[0]
+    for i in range(1, len(args)):
+        result -= i
+    return result
+
+
+def my_sum(*args):
+    result = args[0]
+    for i in range(1, len(args)):
+        result += i
+    return result
+
+
+def my_div(*args):
+    result = args[0]
+    for i in range(1, len(args)):
+        result /= i
+    return result
+
+
+def my_mult(*args):
+    result = args[0]
+    for i in range(1, len(args)):
+        result *= i
+    return result
+
+
+OPERATIONS = {
+    '+': my_sum,
+    '-': my_diff,
+    '/': my_div,
+    '*': my_mult
+}
+
+
+def calc(s):
+    nums = re.split(f"[{''.join(OPERATIONS.keys())}]", s)
+    pos = 
+
+
+print(calc('3+4/2-12'))
